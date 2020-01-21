82d977a04f538fd645120a954226149eea1e4d50
luisguaraca <60004719+luisguaraca@users.noreply.github.com>
2020-01-21 08:24:44 -0500

    cedula y mvc

--- Cedula/src/cedula/Cedula.java
+++ Cedula/src/cedula/Cedula.java
@@ -9,62 +9,100 @@ import java.util.Scanner;
 
 /**
  *
- * @author Sistemas
+ * @author antho
  */
 public class Cedula {
 
     /**
      * @param args the command line arguments
      */
-    public static boolean main(String[] args) {
+    public static void main(String[] args) {
         
         
-boolean cedulaCorrecta = false;
-        System.out.println("Ingrese la cedula");
-        Scanner sc= new Scanner(System.in);
-        String cedula = sc.next();
-try {
- 
-if (cedula.length() == 10) // ConstantesApp.LongitudCedula
-{
-int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
-if (tercerDigito < 6) {
-// Coeficientes de validación cédula
-// El decimo digito se lo considera dígito verificador
- int[] coefValCedula = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
- int verificador = Integer.parseInt(cedula.substring(9,10));
- int suma = 0;
- int digito = 0;
-for (int i = 0; i < (cedula.length() - 1); i++) {
- digito = Integer.parseInt(cedula.substring(i, i + 1))* coefValCedula[i];
- suma += ((digito % 10) + (digito / 10));
-}
- 
-if ((suma % 10 == 0) && (suma % 10 == verificador)) {
- cedulaCorrecta = true;
-}
-else if ((10 - (suma % 10)) == verificador) {
- cedulaCorrecta = true;
-} else {
- cedulaCorrecta = false;
-}
-} else {
-cedulaCorrecta = false;
-}
-} else {
-cedulaCorrecta = false;
-}
-} catch (NumberFormatException nfe) {
-cedulaCorrecta = false;
-} catch (Exception err) {
-System.out.println("Una excepcion ocurrio en el proceso de validadcion");
-cedulaCorrecta = false;
-}
- 
-if (!cedulaCorrecta) {
-System.out.println("La Cédula ingresada es Incorrecta");
-}
-return cedulaCorrecta;
-    
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingrese su número de cédula");
+        String numc = sc.next();
+        String [] numced = numc.split("");
+        String verf = numced[numced.length -1];
+        
+        
+       int dg1,dg2,dg3,dg4,dg5,dg6,dg7,dg8,dg9,dg10;
+       int op1,op2,op3 = 0,op4,op5,op6,op7,op8,op9,op10;
+       int suma =0, res =0, dverf=0;
+       
+        if (numc.length()==10){
+            
+            dg1 = Integer.parseInt(numced[numced.length - 10]);
+            dg2 = Integer.parseInt(numced[numced.length - 9]);
+            dg3 = Integer.parseInt(numced[numced.length - 8]);
+            dg4 = Integer.parseInt(numced[numced.length - 7]);
+            dg5 = Integer.parseInt(numced[numced.length - 6]);
+            dg6 = Integer.parseInt(numced[numced.length - 5]);
+            dg7 = Integer.parseInt(numced[numced.length - 4]);
+            dg8 = Integer.parseInt(numced[numced.length - 3]);
+            dg9 = Integer.parseInt(numced[numced.length - 2]);
+            dverf = Integer.parseInt(numced[numced.length - 1]);
+            
+            System.out.println(""+dg1+""+dg2+""+dg3+""+dg4+""+dg5+""+dg6+""+dg7+""+dg8+""+dg9+""+dverf);
+            //1
+            op1= dg1*2;
+            if(op1>=9){
+            op1 = op1 -9;
+            }
+            //2
+            op2= dg2*1;
+            //3
+            op3 = dg3*2;
+            if(op3>=9){
+            op3 = op3 -9;
+            }
+            
+            //4
+            op4= dg4*1;
+            //5
+            op5= dg5*2;
+            if(op5>=9){
+            op5 = op5 -9;
+            }
+            //6
+            op6= dg6*1;
+            //7
+            op7= dg7*2;
+            if(op7>=9){
+            op7 = op7 -9;
+            }
+            //8
+            op8= dg8*1;
+            //9
+            op9= dg9*2;
+            if(op9>=9){
+            op9 = op9 -9;
+            }
+            
+            suma = op1 +op2+op3+op4+op5+op6+op7+op8+op9;
+            res = suma % 10;
+            
+            if (res==0){
+            if (res == dverf){
+                System.out.println("verdad");
+            }else {
+                System.out.println("CI incorrecta");
+            }
+            }
+            if (res>=1){
+                res = 10-res;
+                if (res == dverf){
+                System.out.println("Digito correcto" );}
+            else {
+                System.out.println("CI incorrecto");
+            }
+        }else {
+                System.out.println("CI incorrecto");
+        }
+        
+            
     }
+    
+}
 }
