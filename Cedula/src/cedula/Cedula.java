c6653064f59240915b1897d8e4bd03cbc28df096
luisguaraca <60004719+luisguaraca@users.noreply.github.com>
2020-01-21 08:14:47 -0500

    Update Cedula.java

--- Cedula/src/cedula/Cedula.java
+++ Cedula/src/cedula/Cedula.java
@@ -5,6 +5,8 @@
  */
 package cedula;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Sistemas
@@ -14,8 +16,55 @@ public class Cedula {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static boolean main(String[] args) {
+        
+        
+boolean cedulaCorrecta = false;
+        System.out.println("Ingrese la cedula");
+        Scanner sc= new Scanner(System.in);
+        String cedula = sc.next();
+try {
+ 
+if (cedula.length() == 10) // ConstantesApp.LongitudCedula
+{
+int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
+if (tercerDigito < 6) {
+// Coeficientes de validación cédula
+// El decimo digito se lo considera dígito verificador
+ int[] coefValCedula = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+ int verificador = Integer.parseInt(cedula.substring(9,10));
+ int suma = 0;
+ int digito = 0;
+for (int i = 0; i < (cedula.length() - 1); i++) {
+ digito = Integer.parseInt(cedula.substring(i, i + 1))* coefValCedula[i];
+ suma += ((digito % 10) + (digito / 10));
+}
+ 
+if ((suma % 10 == 0) && (suma % 10 == verificador)) {
+ cedulaCorrecta = true;
+}
+else if ((10 - (suma % 10)) == verificador) {
+ cedulaCorrecta = true;
+} else {
+ cedulaCorrecta = false;
+}
+} else {
+cedulaCorrecta = false;
+}
+} else {
+cedulaCorrecta = false;
+}
+} catch (NumberFormatException nfe) {
+cedulaCorrecta = false;
+} catch (Exception err) {
+System.out.println("Una excepcion ocurrio en el proceso de validadcion");
+cedulaCorrecta = false;
+}
+ 
+if (!cedulaCorrecta) {
+System.out.println("La Cédula ingresada es Incorrecta");
+}
+return cedulaCorrecta;
     
+    }
 }
