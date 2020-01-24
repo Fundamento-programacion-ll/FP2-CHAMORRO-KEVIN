5c3ab64afdfdc7e23c238476d0a5e9d8fcbcd828
luisguaraca <60004719+luisguaraca@users.noreply.github.com>
2020-01-24 09:49:08 -0500

    deber2

---
+++ deber2/src/deber2/fecha.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deber2;
+
+/**
+ *
+ * @author Sistemas
+ */
+public class fecha {
+    private int dia,mes, anio;
+    
+}
