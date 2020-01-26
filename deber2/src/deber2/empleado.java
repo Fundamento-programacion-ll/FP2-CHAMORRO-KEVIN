f2f6adcb9df401450375bcfba74b5419a8f6587f
kevinchamorro33 <60003927+kevinchamorro33@users.noreply.github.com>
2020-01-26 12:35:55 -0500

    inicio deber 2

--- deber2/src/deber2/empleado.java
+++ deber2/src/deber2/empleado.java
@@ -1,14 +1,86 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package deber2;
 
-/**
- *
- * @author Sistemas
- */
+import java.util.StringTokenizer;
+import javax.swing.JOptionPane;
+
 public class empleado {
+
+    private String nombre;
+    private String apellido;
+    private String fechaIng;
+    private String fechaNac;
+    private double salario;
+
+    public int[][] getVector() {
+        return vector;
+    }
+
+    public void setVector(int[][] vector) {
+        this.vector = vector;
+    }
+    
+    private int vector[][]; 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaIng() {
+        return fechaIng;
+    }
+
+    public void setFechaIng(String fechaIng) {
+        this.fechaIng = fechaIng;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+     private void setVector() {
+        for (int i = 0; i < this.vector.length; i++) {
+            for (int j = 0; j <5; j++) {
+                
+            }
+        }
+    }     
+     
+    public empleado(int dimension) {
+        
+        super();
+        
+        vector=new int [dimension][5];
+        
+        this.setVector();
+        
+    }
+
+    public empleado() {
+    }
+
+   
+    
     
 }
--- deber2/src/deber2/fecha.java
+++ deber2/src/deber2/fecha.java
@@ -1,15 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package deber2;
-
-/**
- *
- * @author Sistemas
- */
 public class fecha {
     private int dia,mes, anio;
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    
+    
+    
     
 }
